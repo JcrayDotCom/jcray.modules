737322511dea8fe7e0ea1fbb9bfab3c5158170b3
gpenverne <gpenverne@gmail.com>
2016-10-02 17:34:44 +0200

    base for using tech env

--- test/features/Context/FeatureContext.php
+++ test/features/Context/FeatureContext.php
@@ -1,18 +1,32 @@
 <?php
 
-use Behat\Behat\Context\Context;
-use Sanpi\Behatch\Context\RestContext;
-use Behat\Gherkin\Node\PyStringNode;
+namespace Context;
 
-class FeatureContext extends RestContext implements Context
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Sanpi\Behatch\Context\BaseContext;
+
+class FeatureContext extends BaseContext implements Context
 {
+    private function getRestContext()
+    {
+        return $this->getMinkContext();
+    }
+
+    public function __construct($user_token, $game_url)
+    {
+        $this->user_token = $user_token;
+        $this->game_url = $game_url;
+    }
+
     /**
      * @When I send a :arg1 request with:
      */
     public function iSendARequestWith($arg1, PyStringNode $string)
     {
-        $url = '/modules/{current_module}/render?game={last_created_game}';
+        $url = '/tech/modules/render?game='.$this->game_url;
+        $this->getRestContext()->iAddHeaderEqualTo('Authorization', 'Bearer '.$this->user_token);
 
-        return $this->iSendARequestToWithBody($arg1, $url, $string);
+        return $this->getRestContext()->iSendARequestToWithBody($arg1, $url, $string);
     }
 }
