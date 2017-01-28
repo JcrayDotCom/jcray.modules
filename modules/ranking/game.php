5739e14804b28cdbd30940b67b13b950106c8dc0
Grégoire <gpenverne@users.noreply.github.com>
2017-01-28 17:11:27 +0100

    Small design and pagination (#18)

--- modules/ranking/game.php
+++ modules/ranking/game.php
@@ -8,12 +8,24 @@ if ($request->get('page')) {
     $page = 1;
 }
 
+$nextPage = false;
+$previousPage = false;
+
 $players = $game->getPlayersByRank($page);
+$nbPlayers = count($players);
+if ($nbPlayers >= 25) {
+    $nextPage = $page + 1;
+}
+if ($page > 1) {
+    $prevousPage = $page - 1;
+}
 
 $arrayReturn = [
     'players' => $players,
     'page' => $page,
     'startRank' => ($page - 1) * 25 + 1,
+    'nextPage' => $nextPage,
+    'previousPage' => $previousPage,
 ];
 
 return $arrayReturn;
